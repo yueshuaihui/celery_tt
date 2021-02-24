6cf0f9e4533a90390ed9591e0e77d3633025ba35
岳帅辉 <yueshuaihui@bonc.com.cn>
2021-02-24 09:41:31 +0800

    11

--- client.py
+++ client.py
@@ -4,10 +4,11 @@ from mm import app
 from celery.app import app_or_default
 
 from celery import Task
+
 b = 2222
 # add.delay(1, 2)
 # add2.delay(3, 4)
 # print("11111")
 a = 1111
 
-list = [1,2,3,4,"a","b","c"]
+list = [1, 2, 3, 4, "a", "b", "c"]
